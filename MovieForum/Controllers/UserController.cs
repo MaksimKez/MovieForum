3c4112f7eba395bd5aad5786efab1b2ac7dc7732
KezMaksim <prostomaxim765@gmail.com>
2024-11-27 18:42:51 +0100

    User Controller

---
+++ MovieForum/Controllers/UserController.cs
@@ -0,0 +1,105 @@
+using Microsoft.AspNetCore.Mvc;
+using MovieForum.BusinessLogic.Models;
+using MovieForum.BusinessLogic.Services.ServicesInterfaces;
+
+namespace MovieForum.Controllers;
+
+[ApiController]
+[Route("api/users")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("{id:guid}", Name = "GetUserById")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<User>> Get(Guid id)
+    {
+        var user = await _userService.GetByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    [HttpGet("search-by-username/{username}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<IEnumerable<User>>> GetByUsername(string username)
+    {
+        var users = await _userService.GetByUsernameAsync(username);
+        if (!users.Any())
+        {
+            return NotFound();
+        }
+
+        return Ok(users);
+    }
+
+    [HttpGet("search-by-email/{email}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<User>> GetByEmail(string email)
+    {
+        var user = await _userService.GetByEmailAsync(email);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Guid>> Add(User user)
+    {
+        var id = await _userService.AddAsync(user);
+        if (id == Guid.Empty)
+        {
+            return BadRequest("Unable to create the user.");
+        }
+
+        return CreatedAtRoute(
+            routeName: "GetUserById",
+            routeValues: new { id },
+            value: id
+        );
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> Update(User user)
+    {
+        var isUpdated = await _userService.UpdateAsync(user);
+        if (!isUpdated)
+        {
+            return BadRequest("Unable to update the user.");
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var isDeleted = await _userService.DeleteAsync(id);
+        if (!isDeleted)
+        {
+            return BadRequest("Unable to delete the user.");
+        }
+
+        return NoContent();
+    }
+}
