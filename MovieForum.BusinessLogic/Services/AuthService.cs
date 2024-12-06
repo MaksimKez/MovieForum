51ea5574b78477acf787432ade7b2a42e7e0d8af
KezMaksim <prostomaxim765@gmail.com>
2024-12-07 00:42:05 +0100

    refresh token attempt 1

--- MovieForum.BusinessLogic/Services/AuthService.cs
+++ MovieForum.BusinessLogic/Services/AuthService.cs
@@ -1,4 +1,6 @@
+using System.IdentityModel.Tokens.Jwt;
 using AutoMapper;
+using MovieForum.BusinessLogic.auth;
 using MovieForum.BusinessLogic.auth.Interfaces;
 using MovieForum.BusinessLogic.Models;
 using MovieForum.BusinessLogic.Services.ServicesInterfaces;
@@ -16,14 +18,15 @@ public class AuthService : IAuthService
     private readonly IJwtProvider _jwtProvider;
     private readonly UserValidator _validator;
 
-    public AuthService(IUserRepository userRepository, IPasswordHasher passwordHasher, IMapper mapper, IJwtProvider jwtProvider)
+    public AuthService(IUserRepository userRepository, IPasswordHasher passwordHasher, IMapper mapper, IJwtProvider jwtProvider, UserValidator validator)
     {
         _userRepository = userRepository;
         _passwordHasher = passwordHasher;
         _mapper = mapper;
         _jwtProvider = jwtProvider;
+        _validator = validator;
     }
-    
+
     public async Task<bool> RegisterAsync(User user)
     {
         if (user.PasswordHash == null)
@@ -51,25 +54,57 @@ public class AuthService : IAuthService
         var id = await _userRepository.AddAsync(userEntity);
         return id != Guid.Empty;
     }
-    
-    public async Task<string?> LoginAsync(string email, string password)
+
+    public async Task<TokensModel> LoginAsync(string email, string password)
     {
         var user = await _userRepository.GetByEmailAsync(email);
         if (user?.PasswordHash == null || user.PasswordSalt == null)
         {
-            //todo log
+            // todo log
             return null;
         }
-        //var isValidPassword = _passwordHasher.VerifyPassword(password, user.PasswordHash, user.PasswordSalt);
-        var isValidPassword = _passwordHasher.VerifyPassword(password, user.PasswordHash);
 
+        var isValidPassword = _passwordHasher.VerifyPassword(password, user.PasswordHash);
         if (!isValidPassword)
         {
-            //todo log
+            // todo log
             return null;
         }
+
+        var accessToken = _jwtProvider.GenerateJwtToken(user.Email, user.Id);
+
+        var refreshToken = _jwtProvider.GenerateJwtToken(user.Email, user.Id, isRefreshToken: true);
+
+        //todo add refresh token to user
+        await _userRepository.AddRefreshTokenAsync(user.Id, refreshToken);
         
-        var token = _jwtProvider.GenerateJwtToken(user.Email, user.Id);
-        return token;
+
+        return new TokensModel
+        {
+            AccessToken = accessToken,
+            RefreshToken = refreshToken
+        };
+    }
+
+    public async Task<string?> RefreshAccessTokenAsync(string refreshToken)
+    {
+        if (!_jwtProvider.ValidateJwtToken(refreshToken))
+        {
+            throw new UnauthorizedAccessException("Invalid refresh token.");
+        }
+
+        var claims = new JwtSecurityTokenHandler().ReadJwtToken(refreshToken).Claims;
+        var userId = new Guid(claims.FirstOrDefault(c => c.Type == "UserId")?.Value);
+
+        // Проверка существования refresh token в БД
+        var storedRefreshToken = await _userRepository.GetRefreshTokenAsync(userId);
+        if (storedRefreshToken != refreshToken)
+        {
+            throw new UnauthorizedAccessException("Refresh token mismatch.");
+        }
+
+        var accessToken = _jwtProvider.GenerateJwtToken(claims.First(c => c.Type == "Email").Value, userId);
+
+        return accessToken;
     }
 }
---
+++ MovieForum.BusinessLogic/auth/TokensModel.cs
@@ -0,0 +1,7 @@
+namespace MovieForum.BusinessLogic.auth;
+
+public class TokensModel
+{
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
